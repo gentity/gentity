ee6ce1f1e5e478238bcc9bdf776442390e23026a
Pachler, Uwe <uwe.pachler@datagroup.de>
2019-02-13 01:17:06 +0100

    Fixed NPE when attempting to unbind from a null reference

---
+++ gentity-lib/src/test/java/onetoone/Dog.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2019 The Gentity Project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package onetoone;
+
+import com.github.gentity.ToOneSide;
+
+/**
+ *
+ * @author upachler
+ */
+public class Dog {
+	private Kennel kennel;
+	static final ToOneSide<Dog,Kennel> relationTo$kennel = ToOneSide.of(m -> m.kennel, (m,o) -> m.kennel = o, Kennel.relationTo$dog);
+
+	public Kennel getKennel() {
+		return relationTo$kennel.get(this);
+	}
+
+	
+	
+}
